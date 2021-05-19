90e70d1ccaa7a04d0e3397836ee01fe9933380f6
Aleksandr Zagoskin <zaleksandrne@gmail.com>
2021-05-19 12:00:15 +0300

    setup.cfg

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -18,7 +18,5 @@ class StaticPagesURLTests(TestCase):
         """Проверка контекста страниц."""
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
-
-
         response = self.guest_client.get('/second_page/')
         self.assertContains(response, 'А это вторая страница!')
