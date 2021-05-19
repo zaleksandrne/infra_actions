00a36ec1956495f5d5942dad89ef63be5744e931
Aleksandr Zagoskin <zaleksandrne@gmail.com>
2021-05-19 12:05:21 +0300

    setup.cfg

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -10,7 +10,6 @@ class StaticPagesURLTests(TestCase):
         """Проверка доступности страниц."""
         response = self.guest_client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
-
         response = self.guest_client.get('/second_page/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
